f78cf1f34c71fc78c996a1ba5cd7885236e32302
ainekeenan <keenancaine@gmail.com>
2023-05-20 13:17:10 -0700

    working on API for finding recipes rareingredient

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import urllib.request, json, os
 
 app = Flask(__name__)
 
@@ -6,6 +7,29 @@ app = Flask(__name__)
 
 def hello_world():
     return "<p>Hello, World! CATSJKLSKLKLJSLSK</p>"
+
+def from_url_get_ingredient():
+    # request recipes with Rare ingredients variable
+    #Recipes requested = 3
+    # ignore common pantry items bc they are common
+    url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients={}&number=3&ignorePantry=true".format(os.environ.get("Rare_Ingredients"))
+    response = urllib.request.urlopen(url)
+    recipes = response.read()
+    dict = json.load(recipes)
+
+    # make empty array for recipes (can use recipes bc you loaded it into dict)
+    recipes = []
+
+
+    # get the title of recipes
+    for recipe in dict[]{
+        recipe = {
+            "recipeName" = recipe["title"]
+            
+        }
+
+    }
+    
 
 
 if  __name__ == '__main__':
