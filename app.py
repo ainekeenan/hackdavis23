0fa7fcb95ae1e98c63bc0b36d4b2bea80c387ae7
ritakura <reina.itk@gmail.com>
2023-05-20 12:59:58 -0700

    start frontend

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import cgi, cgitb
 
-app = 
+app = Flask(__name__)
+
+forms=cgi.FieldStorage()
+recipe_url = ""
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        recipe_url = request.form.get("url")
+        print(recipe_url)
+    return render_template('home.html', url=recipe_url)
+
+if __name__ == '__main__':
+    app.run()
+
 
