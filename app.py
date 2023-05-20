abbfa852cbbc7745befce3862573d3696dcc233e
ainekeenan <keenancaine@gmail.com>
2023-05-20 16:55:04 -0700

    correct api call

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def from_url_get_otherRareRecipes():
     # ignore common pantry items bc they are common
     health = "give me the health"
 
-    url = "https://api.edamam.com/api/recipes/v2?app_id=fd727a17&app_key=3db153f6a682953219a9bb02b92537f9&q={}&health=".format(os.environ.get("Rare_Ingredients"))
+    url = "https://api.edamam.com/api/recipes/v2?app_id=fd727a17&app_key=3db153f6a682953219a9bb02b92537f9&q={}&health={}".format(os.environ.get("Rare_Ingredients"), health)
     response = urllib.request.urlopen(url)
     recipes = response.read()
     dict = json.loads(recipes)
